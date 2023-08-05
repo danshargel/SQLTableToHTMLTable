ecf5bf746a03bb1d8d1f04a9c324e14c7bdda2a9
Dan Shargel <danshargel@gmail.com>
2023-08-05 10:36:05 -0600

    Update 03_ExampleExecution.sql

--- 03_ExampleExecution.sql
+++ 03_ExampleExecution.sql
@@ -10,22 +10,49 @@ SELECT TOP 50 *
   FROM master.sys.objects
  ORDER BY name;
 
+ DROP TABLE IF EXISTS #tbTestMail;
+SELECT TOP 50 *
+  INTO #tbTestMail
+  FROM master.sys.objects
+ ORDER BY name;
+
 
 SET NOCOUNT ON;
 
 DECLARE @HTMLMail VARCHAR(MAX),
         @Message  VARCHAR(MAX);
 
+---------------------------------------
+-- Send using temp table
+---------------------------------------
+EXEC DBAdmin.dbo.SQLTableToHTMLTable @TableName = '#tbTestMail',
+                                     @ColumnsToExclude = 'RowID',
+                                     @HTML = @HTMLMail OUTPUT;
+
+SELECT @Message = '<h3>Hey look at this from #tbTestMail.</h3><br><br>' + @HTMLMail;
+
+EXEC msdb.dbo.sp_send_dbmail @profile_name = DEFAULT,
+                             @body_format = 'HTML',
+                             @recipients = 'email@xyz.com',
+                             @subject = 'test email #tbTestMail',
+                             @body = @Message;
+
+DROP TABLE IF EXISTS #tbTestMail;
+
+
+---------------------------------------
+-- Send using permanent table
+---------------------------------------
 EXEC DBAdmin.dbo.SQLTableToHTMLTable @TableName = 'DBAdmin.temp.tbTestMail',
                                      @ColumnsToExclude = 'RowID',
                                      @HTML = @HTMLMail OUTPUT;
 
-SELECT @Message = '<h3>Hey look at this, dummy.</h3><br><br>' + @HTMLMail;
+SELECT @Message = '<h3>Hey look at this from DBAdmin.temp.tbTestMail.</h3><br><br>' + @HTMLMail;
 
 EXEC msdb.dbo.sp_send_dbmail @profile_name = DEFAULT,
                              @body_format = 'HTML',
-                             @recipients = 'xyz@xyz.com',
-                             @subject = 'test email',
+                             @recipients = 'email@xyz.com',
+                             @subject = 'test email DBAdmin.temp.tbTestMail',
                              @body = @Message;
 
-DROP TABLE IF EXISTS temp.tbTestMail;
+DROP TABLE IF EXISTS DBAdmin.temp.tbTestMail;
